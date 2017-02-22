19117ef4598fa4253fcda6ca0ccd57d282758c4b
Oren Dobzinski <oren@desk.com>
2017-02-22 15:53:29 -0500

    extract city

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -8,9 +8,18 @@ exports.handle = (client) => {
             return Boolean(client.getConversationState().weatherCity)
         },
 
+        extractInfo() {
+            const city = client.getFirstEntityWithRole(client.getMessagePart(), 'city')
+            if (city) {
+                client.updateConversationState({
+                    weatherCity: city,
+                })
+                console.log('User wants the weather in:', city.value)
+            }
+        },
+
         prompt() {
-            // Need to prompt user for city
-            console.log('Need to ask user for city')
+            client.addResponse('prompt/weather_city')
             client.done()
         },
     })
